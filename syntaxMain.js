7408fe1cae243d287897b09329d038fa90b08ce2
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2016-03-12 20:31:41 +0000

    Testing the simplest stage...

---
+++ syntaxMain.js
@@ -0,0 +1,13 @@
+/* *\
+*   * Name: Syntax 0.0.1
+*   * Creator: Sabri Meeran
+*   * Licence: Creative Commons Attribution-ShareAlike 4.0 International Licence
+\* */
+
+var a = "*: lowercase";
+var b = 0;
+var c;
+while (a[b] !== ":" || b === a.length) {
+  b++;
+  c += a[b];
+};
