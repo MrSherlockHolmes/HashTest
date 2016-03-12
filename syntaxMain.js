91bd2613bb2d222a985aba69d1dc8cfb7ba0a638
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2016-03-12 20:51:56 +0000

    Update syntaxMain.js

--- syntaxMain.js
+++ syntaxMain.js
@@ -4,10 +4,11 @@
 *   * Licence: Creative Commons Attribution-ShareAlike 4.0 International Licence
 \* */
 
-var a = "*: lowercase";
+var a = "div: load";
 var b = 0;
-var c;
-while (a.charAt(b) !== ":" || b === a.length) {
-  c += a.charAt(b);
+var c = "";
+while (a.charAt(b) !== ":") {
+    c = c.concat(a.charAt(b));
     b++;
 };
+console.log(c);
